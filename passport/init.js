781f01752ba605048697faa3c1fd03b82d8c5c8d
Sohyeon Lee <sohyeonlee@berkeley.edu>
2014-10-03 20:22:19 -0700

    almost

--- passport/init.js
+++ passport/init.js
@@ -1,21 +1,24 @@
 var login = require('./login');
 var signup = require('./signup');
-var User = require('../models/user')
+var User = require('../models/user');
 
 module.exports = function(passport){
-	
-	passport.serializeUser(function(user, done) {
-		console.log('serializing user: ', user);
-		done(null, user._id);
-	});
-	
-	passport.deserializeUser(function(id, done) function {
-		User.findById(id, function(err, user) {
-			console.log('deserializing user: ', user);
-			done(err, user);
-		});
-	});
-	
-	login(passport);
-	signup(passport);
+
+	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function(user, done) {
+        console.log('serializing user: ');console.log(user);
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            console.log('deserializing user:',user);
+            done(err, user);
+        });
+    });
+
+    // Setting up Passport Strategies for Login and SignUp/Registration
+    login(passport);
+    signup(passport);
+
 }
